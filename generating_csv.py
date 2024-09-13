6064799589669bf463586bed190c169f6ed58721
Tarun rk <tarunrk2002@gmail.com>
2024-09-13 13:56:12 +0530

    random data is transferred to csv file

--- generating_csv.py
+++ generating_csv.py
@@ -2,7 +2,7 @@ from PIL import Image
 import pytesseract
 import re
 import random
-class abc:
+class address:
     def creating_random_address(self):
         r_number = random.randint(000,999)
         r_streetnames = random.choice(['Main St', 'Oak St', 'Pine St', 'Maple St', 'Elm St', 'Cedar St'])
@@ -10,6 +10,63 @@ class abc:
         r_states = random.choice(['IL', 'MA', 'CA', 'NY', 'TX'])
         r_zipcodes = random.choice(['62704', '02130', '90001', '10001', '60601', '75201'])
         return(f"{r_number},{r_streetnames},{r_citynames},{r_states}-{r_zipcodes}")
+
+    def creating_non_adddress(self):
+        non_address_texts = random.choice([
+            "Bachelor of Science in Computer Science",
+            "Software Engineer at XYZ Company",
+            "Experience in developing web applications",
+            "Master of Business Administration",
+            "Certified Data Scientist",
+            "Fluent in Python and Java",
+            "Bachelor of Arts in Graphic Design",
+            "Front-End Developer at ABC Tech",
+            "Proficient in React, JavaScript, and CSS",
+            "Master of Science in Information Technology",
+            "Certified Scrum Master",
+            "Experienced in cloud architecture and DevOps practices",
+            "Fluent in SQL and C#",
+            "Software Architect at DEF Innovations",
+            "Bachelor of Engineering in Electronics",
+            "Agile project management expert",
+            "Proficient in AI and Machine Learning",
+            "Database Administrator with 5+ years of experience",
+            "Master of Science in Cybersecurity",
+            "Certified AWS Solutions Architect",
+            "Expertise in mobile app development (iOS/Android)",
+            "I am cooking",
+            "The sun is shining brightly",
+            "She loves reading mystery novels",
+            "We're going on a vacation next week",
+            "He just finished his workout",
+            "The coffee tastes amazing",
+            "They are watching a movie tonight",
+            "It's raining outside",
+            "I need to buy groceries",
+            "He is learning to play the guitar",
+            "The project deadline is tomorrow",
+            "I am baking a cake",
+            "She is working from home today",
+            "We're going for a hike this weekend",
+            "I am cleaning the house",
+            "The dog is barking loudly",
+            "I am writing an email",
+            "She is practicing yoga",
+            "The kids are playing in the yard",
+            "I need to finish this book",
+            "He is preparing dinner",
+            "The meeting starts at 3 PM",
+            "She is planning a surprise party",
+            "I am watering the plants",
+            "They are building a new house",
+            "I just finished a great movie",
+            "She is painting her bedroom",
+            "The train is arriving soon",
+            "I am learning to code",
+            "We are organizing a charity event"
+        ])
+        return(non_address_texts)
+
 
 
 
--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-from generating_csv import abc
+import random
+import pandas as pd
 
-a = abc()
-a.creating_random_address()
+from generating_csv import address
+
+address_class = address()
+
 
 address = []
 is_address = []
+non_address = []
+is_non_address = []
+
+
 for i in range(1000):
-    address.append(a.creating_random_address())
+    address.append(address_class.creating_random_address())
     is_address.append(1)
+    non_address.append(address_class.creating_non_adddress())
+    is_non_address.append(0)
+
+
 
 actuall_add = list(zip(address,is_address))
-print(actuall_add)
+actuall_nonadd = list(zip(non_address,is_non_address))
+
+mixed_data = actuall_add + actuall_nonadd
+
+random.shuffle(mixed_data)
+
+
+shuffled_data = mixed_data
+
+df = pd.DataFrame(shuffled_data)
+df.to_csv("shuffled_data.csv",index=False)
+print("done")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
